fb0e589b54dbda9293cb058320b3f8344455cd97
stillistica <rozhkovavika24@gmail.com>
2025-03-10 10:51:12 +0200

    fix swiper

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -40,71 +40,68 @@ const btnNext = document.querySelector('.swiper-button-next');
 let isError = false;
 
 document.addEventListener('DOMContentLoaded', async () => {
-	try {
-		const result = await fetchReviews();
+  try {
+    const result = await fetchReviews();
 
-		if (!result || result.length === 0) {
-			notFoundMes.classList.remove('hidden');
-			notFoundMes.classList.add('css-err-mes');
-			btnNext.setAttribute('disabled', 'true');
-			isError = true;
-			return;
-		}
+    if (!result || result.length === 0) {
+      notFoundMes.classList.remove('hidden');
+      notFoundMes.classList.add('css-err-mes');
+      btnNext.setAttribute('disabled', 'true');
+      isError = true;
+      return;
+    }
 
-		reviewsList.innerHTML = renderReviews(result);
+    reviewsList.innerHTML = renderReviews(result);
 
-		const swiper = new Swiper('.swiper', {
-			direction: 'horizontal',
-			loop: false,
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: false,
+      spaceBetween: 16,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      mousewheel: {
+        invert: true,
+      },
+      resizeObserver: true,
+      slidesPerView: 1,
+      breakpoints: {
+        768: { slidesPerView: 2 },
+        1440: { slidesPerView: 4 },
+      },
+    });
 
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: 'swiper-button-prev',
-			},
-				spaceBetween: 16,
-				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-        
-				},
-				keyboard: {
-					enabled: true,
-					onlyInViewport: true,
-				},
-				mousewheel: {
-					invert: true,
-				},
-				resizeObserver: true,
-				slidesPerView: 1,
-				breakpoints: {
-					768: { slidesPerView: 2 },
-					1440: { slidesPerView: 4 },
-				},
-			});
+    function updateButtonSlider() {
+      btnPrev.toggleAttribute('disabled', swiper.isBeginning);
+      btnNext.toggleAttribute('disabled', swiper.isEnd);
+    }
 
-		function updateButtonSlider() {
-			btnPrev.toggleAttribute('disabled', swiper.isBeginning);
-			btnNext.toggleAttribute('disabled', swiper.isEnd);
-		};
-
-		swiper.on('slideChange', updateButtonSlider);
-		updateButtonSlider();
-	} catch (error) {
-		isError = true;
-		console.error(error);
-	}
+    swiper.on('slideChange', updateButtonSlider);
+    updateButtonSlider();
+  } catch (error) {
+    isError = true;
+    console.error(error);
+  }
 });
 
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (isError && entry.isIntersecting) {
-			iziToast.error({
-				title: 'Error',
-				message: 'No feedback data about reviews. We are solving the problem',
-				position: 'topRight',
-			})
-		}
-	});
-}, { threshold: 0.2 });
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (isError && entry.isIntersecting) {
+        iziToast.error({
+          title: 'Error',
+          message: 'No feedback data about reviews. We are solving the problem',
+          position: 'topRight',
+        });
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
 
 observer.observe(reviewsContainer);
