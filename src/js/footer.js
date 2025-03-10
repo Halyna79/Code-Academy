8cbce9d88040cc797344c4b5d1be7b242af9b91b
unknown <ivannabelichenko@gmail.com>
2025-03-10 15:56:21 +0200

    2

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const form = document.querySelector(".footer-form")
+const btnClose = document.querySelector(".btn-close-res");
+    const menu = document.querySelector(".modal-window");
+
 
 const baseUrl = "https://portfolio-js.b.goit.study/api";
 const endPoint = "/requests";
@@ -12,24 +15,43 @@ form.addEventListener('submit', async (e) => {
         const comment = e.currentTarget.elements.comments.value.trim();
 
         const data = JSON.stringify({email,comment})
-        alert(data)
+        
         const url = baseUrl + endPoint
-        alert(url)
+        
         const response = await axios.post(url,data,{
             headers: {
                 "accept": "application/json",
                 "Content-Type": "application/json"}
         })
 
-        alert(111);    
+       
         alert(response.data.title);
+        
+        menu.classList.add('show');
+
+        form.reset()
+
+        menu.addEventListener("click", e=> {
+            menu.classList.remove('show');
+        })
+        
+        document.addEventListener('keydown', e => {
+            if (e.key === "Escape") menu.classList.remove('show');
+        }); 
+
+        btnClose.addEventListener ('click', (e) => {
+            menu.classList.remove('show');
+        })
+
     }catch(e){
-        alert(e)
+        alert(response)
     }    
     
     
 });
 
-
-
-
+    
+    
+  
+    
+    
